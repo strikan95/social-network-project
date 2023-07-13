a35a75fadb353916cf5d90e9681c1ef8f7dace3e
Dominik Gubo <gubo.dominik@gmail.com>
2023-07-13 13:39:41 +0200

    Initialized Feed view

---
+++ src/Controller/FeedController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FeedController extends AbstractController
+{
+    #[Route('/feed', name: 'app_feed')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+
+        $posts = $entityManager->getRepository(Post::class)->findBy(['access' => 'Public']);
+
+        return $this->render('pages/feed_page.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+}
