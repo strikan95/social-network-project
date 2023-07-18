2530e53ed112976851d7762d9e02fa8e1a82bcdb
Dominik Gubo <gubo.dominik@gmail.com>
2023-07-18 21:51:10 +0200

    Added profile-show route

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,12 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Profile;
+use App\Entity\User;
 
 class ProfileController extends AbstractController
 {
 
     #[Route('/profile', name: 'app_profile')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(): Response
     {
         $user = $this->getUser();
         $userProfile = $user->getProfile();
@@ -20,6 +21,22 @@ class ProfileController extends AbstractController
         return $this->render('pages/profile_page.html.twig', [
             'user' => $user,
             'profile' => $userProfile
+        ]);
+    }
+
+    #[Route('/profile/{id}', name: 'app_profile_show')]
+    public function show(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $user = $entityManager->getRepository(User::class)->find($id);
+        $userProfile = $user->getProfile();
+        $currentId = $this->getUser()->getId();
+
+        if($id == $currentId ) return $this->redirect('/profile');
+
+        return $this->render('pages/profile_page.html.twig', [
+            'user' => $user,
+            'profile' => $userProfile,
+            'show' => 'true'
         ]);
     }
 }
