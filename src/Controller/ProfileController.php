d4d53e7bab53d30e710eef948e02f674330995ad
Dominik Gubo <gubo.dominik@gmail.com>
2023-07-19 18:45:27 +0200

    Initialized image upload functionality in settings

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,8 +8,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Profile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use App\Entity\User;
 use App\Form\SettingsType;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProfileController extends AbstractController
 {
@@ -37,30 +40,69 @@ class ProfileController extends AbstractController
 
         return $this->render('pages/profile_page.html.twig', [
             'user' => $user,
-            'profile' => $userProfile,
+            'profile' => $userProfile, 
             'show' => 'true'
         ]);
     }
 
     #[Route('/settings', name: 'app_settings')]
-    public function getSettings(EntityManagerInterface $entityManager, Request $request): Response
+    public function settings(EntityManagerInterface $entityManager, Request $request, SluggerInterface $slugger): Response
     {
 
         $user = $this->getUser();
         $profile = $user->getProfile();
-
         $form = $this->createForm(SettingsType::class, $profile);
+
         $form->handleRequest($request);
+
         if($form->isSubmitted() && $form->isValid() ){
-            $profile = $form->getData();
+
+            $formBackground = $form->get('backgroundImage')->getData();
+            $formImage = $form->get('profileImage')->getData();
+
+            if($formBackground){
+                $originalFilename = pathinfo($formBackground->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-background-'.$user->getId().'.'.$formBackground->guessExtension();
+
+                $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
+
+                $profile->setBackgroundImage($newFilename);
+
+                try {
+                    $formBackground->move($destination, $newFilename);
+                } catch (FileException $e) {
+                   return $e;
+                }
+            }
+
+            if($formImage){
+                $originalFilename = pathinfo($formImage->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-image-'.$user->getId().'.'.$formImage->guessExtension();
+
+                $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
+                
+                $profile->setProfileImage($newFilename);
+
+                try {
+                    $formImage->move($destination, $newFilename);
+                } catch (FileException $e) {
+                   return $e;
+                }
+            }
+
+
             $entityManager->persist($profile);
             $entityManager->flush();
+
+            
             return $this->redirect($request->getUri());
         }
 
         return $this->render('pages/settings_page.html.twig', [
             'user' => $user,
-            'form' => $form
+            'form' => $form,
         ]);
     }
 }
--- src/Form/SettingsType.php
+++ src/Form/SettingsType.php
@@ -4,32 +4,47 @@ namespace App\Form;
 
 use App\Entity\Profile;
 use App\Controller\ProfileController;
+use App\Form\DataTransformer\FileToPathTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class SettingsType extends AbstractType
 {
+    private string $uploadPath;
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
         $builder
             ->add('briefDescription', TextareaType::class, [
                 'attr' => ['placeholder' => 'Introduce your profile'],
-                'label' => 'Introduction',
+                'label' => 'Introduction'
             ])
             ->add('description', TextareaType::class, [
                 'attr' => ['placeholder' => 'Describe yourself']
             ])
-            ->add('backgroundImage', FileType::class, [
-                'label' => 'Background'
+            ->add('backgroundImage', FileType::class,  [
+                'label' => 'Background',
+                'required' => false,
+                'data_class' => null,
+                'constraints' => [
+                    new File(['mimeTypes' => ['image/png', 'image/jpeg'], 'mimeTypesMessage' => 'Please upload only image formats'])
+                ]
             ])
-            ->add('profileImage', FileType::class)
+            ->add('profileImage', FileType::class, [
+                'required' => false,
+                'data_class' => null,
+                'constraints' => [
+                    new File(['mimeTypes' => ['image/png', 'image/jpeg'], 'mimeTypesMessage' => 'Please upload only image formats'])
+                ]
+            ])
             ->add('submit', SubmitType::class)
         ;
-    }
+  }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
@@ -37,4 +52,5 @@ class SettingsType extends AbstractType
             'data_class' => Profile::class,
         ]);
     }
+
 }
